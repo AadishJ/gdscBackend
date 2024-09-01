80611642f86c5dcb1ef7b41defa511d2f594e6bc
Aadish Jain <jain.aadishj@gmail.com>
2024-09-01 17:51:53 +0530

    Deployment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectMongoDB = require( './config/connect' );
 const PORT = process.env.PORT || 5000;
-connectMongoDB( "mongodb://localhost:27017/gdsc" ).then( () => console.log( "MongoDB connected" ) )
+connectMongoDB(process.env.DATABASE).then( () => console.log( "MongoDB connected" ) )
 app.use( express.urlencoded( { extended: false } ) );
 app.use( cors() );
 app.use(bodyParser.json());
